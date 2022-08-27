4aa1c3198f0918d6cd288ac39bdfa3aba2cda92e
Eamon <esh2464@email.vccs.edu>
2022-08-27 14:30:51 -0400

    commit tree 70a97e6a693c7b1947f3a47d16d230a402b65c2b

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// const cards = ['Guadalupe', 'Ollie', 'Aki'];
+
+const phrases = []
+
+function writeCards(cards, event) {
+  for (let i = 0; i < cards.length; i++) {
+    phrases.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return(phrases);
+
+}
+
+function countDown(start) {
+  for (let i = start; i > -1; i--) {
+  console.log(i);
+
+} } 
+
+countDown(11); 
